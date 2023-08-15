15d69ac43b0e250f2d50322507c6d707714bb184
Westy-inc <65766828+Westy-inc@users.noreply.github.com>
2023-08-15 15:01:21 +0100

    house cleaning
    just cleaning up code
    and using pylint-flask

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,15 +1,16 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user,LoginManager, login_required , logout_user, current_user
 
 
+basedir = os.path.abspath(os.path.dirname(__file__)) 
 
-basedir = os.path.realpath(os.path.dirname(__file__))
-
-app = Flask (__name__,template_folder='templates')
+app = Flask (__name__)
 app.config['SECRET_KEY']  = 'djflkdjflksflk' #crf key to be moved to config file
 app.config['SQLALCHEMY_DATABASE_URI' ] =\
         'sqlite:///' + os.path.join(basedir, 'task.db')
+app.config ['SQLALCHEMY_BINDS'] = {'auth' : 'sqlite:///' + os.path.join(basedir, 'users.db')}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
--- flaskr/models.py
+++ flaskr/models.py
@@ -1,22 +1,19 @@
 from app import app, db
-from flask_login import UserMixin
-
-from flask_login import UserMixin, login_user,LoginManager, login_required , logout_user, current_user
-
+#from flask_login import UserMixin
+from flask_login import UserMixin,LoginManager
 
 #setting up db
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    body = db.Column(db.String, nullable=False)
     date = db.Column(db.DateTime(timezone=True), nullable=False)
-    public = db.Column(db.Boolean, default=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     
     def __repr__(self):
-        return f'<Student {self.title} was created {self.date}>'
+        return f'<user {self.title} was created {self.date}>'
     
 class User(db.Model,UserMixin ):
+#    __bind_key__ = "auth"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable =False)
