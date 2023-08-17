facadc4c561d7f89e4a0d939ab6508c5710b8bcf
Westy-inc <65766828+Westy-inc@users.noreply.github.com>
2023-08-17 14:03:39 +0100

    file layout update

---
+++ flaskr/app.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+basedir = os.path.realpath(os.path.dirname(__file__))
+
+app = Flask (__name__,template_folder='templates')
+app.config['SECRET_KEY']  = 'djflkdjflksflk' #crf key to be moved to config file
+app.config['SQLALCHEMY_DATABASE_URI' ] =\
+        'sqlite:///' + os.path.join(basedir, 'task.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+
+
+
+db = SQLAlchemy(app) #db instances
+app.app_context().push() #for setting up db file via terminal
+
+
+
+from routes import *
+
+
+
+if __name__ == '__main__':
+        app.run(debug=True)
---
+++ flaskr/models.py
@@ -0,0 +1,32 @@
+from app import app, db
+from flask_login import UserMixin
+
+from flask_login import UserMixin, login_user,LoginManager, login_required , logout_user, current_user
+
+
+#setting up db
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    body = db.Column(db.String, nullable=False)
+    date = db.Column(db.DateTime(timezone=True), nullable=False)
+    public = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def __repr__(self):
+        return f'<Student {self.title} was created {self.date}>'
+    
+class User(db.Model,UserMixin ):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable =False)
+    email = db.Column(db.String, nullable=False)
+    tasks = db.relationship('Task', backref='user', lazy=True)
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view='login'
+@login_manager.user_loader   #manager for logining user in 
+def load_user(user_id):
+    return User.query.get(int(user_id))
